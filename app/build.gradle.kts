64b90a4f2a3e74ed88d600a5c0e033dfe7dd2934
Helena <hvanlooveren@conexsante.com>
2024-09-11 15:55:30 +0200

    Fixed Gradle buildscript

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("kotlin-android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -30,11 +31,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    kotlin {
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
     buildFeatures {
         compose = true
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,10 @@
-
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-}
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     val kotlinVersion by extra("1.9.0")
-    val gradleVersion by extra("8.5.1")
+    val gradleVersion by extra("8.5.2")
     val googleServicesVersion by extra("4.3.14")
 
     dependencies {
