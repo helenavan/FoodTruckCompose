7ae55669b187b3b338190b880231a9080683beef
Helena <hvanlooveren@conexsante.com>
2024-12-11 14:06:10 +0100

    New versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.toulousehvl.myfoodtruck"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
